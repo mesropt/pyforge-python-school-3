f9933c3e372d11cc8a44040931b097dc5dfeed28
Mesrop Tarkhanyan <mesroptnamak@gmail.com>
2024-09-29 14:57:14 +0400

    Small corrections

--- src/main.py
+++ src/main.py
@@ -67,12 +67,6 @@ molecules = {
         "weight": 58.44247,
         "formula": "ClNa"
     },
-    10: {
-        "name": "Sodium chloride",
-        "smiles": "[Na+].[Cl-]",
-        "weight": '58.44247',
-        "formula": "ClNa"
-    }
 }
 
 UPLOAD_DIR = "./uploads"
